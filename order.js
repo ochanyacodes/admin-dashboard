3a03603da86097bb20ef6e79282f2440957d6659
ochanyacodes <162470931+ochanyacodes@users.noreply.github.com>
2024-08-17 23:32:05 +0100

    Add files via upload

---
+++ order.js
@@ -0,0 +1,20 @@
+ const Orders = [
+    {
+        productName: 'JavaScript',
+        productNumber: '54647',
+        PaymentStatus:'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS ',
+        productNumber: '564347',
+        PaymentStatus:'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box',
+        productNumber: '98047',
+        PaymentStatus:'Paid',
+        status: 'Active'
+    },
+ ]
